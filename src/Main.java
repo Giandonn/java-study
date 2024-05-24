58a0a097ec78220b2f43ca903a9a109b7708bdd7
Giandonn <lucas_raineri04@hotmail.com>
2024-05-24 09:33:14 -0300

    upt

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Screen Match!");
+
+        int anoLancamento = 2022;
+        double notaFilme = 8.15;
+        boolean incluidoPlano = false;
+        System.out.printf("Filme: Top Gun Maverick" + " Ano de Lançamento: " + anoLancamento + " Nota do Filme: " + notaFilme);
+    }
+}
